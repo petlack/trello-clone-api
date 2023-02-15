a7299134810315a65fa527821203d1b412068737
peter@output.sk <peter@output.sk>
2023-02-15 21:09:02 +0200

    Update queries & mutations

--- api/mutations.py
+++ api/mutations.py
@@ -2,14 +2,26 @@ from ariadne import convert_kwargs_to_snake_case
 from .db import (
     create_task,
     move_task,
+    delete_task,
+    create_board,
 )
 
 @convert_kwargs_to_snake_case
-def create_task_resolver(obj, info, board_id, description, badge):
+def create_board_resolver(obj, info, title):
+    payload = create_board(title)
+    return payload
+
+@convert_kwargs_to_snake_case
+def create_task_resolver(obj, info, board_id, description, badge=None):
     payload = create_task(board_id, description, badge)
     return payload
 
 @convert_kwargs_to_snake_case
 def move_task_resolver(obj, info, board_id, task_id, position):
     payload = move_task(board_id, task_id, position)
+    return payload
+
+@convert_kwargs_to_snake_case
+def delete_task_resolver(obj, info, task_id):
+    payload = delete_task(task_id)
     return payload
--- api/queries.py
+++ api/queries.py
@@ -1,4 +1,9 @@
+import datetime
+
 from .db import eager_find_all_tasks, eager_find_all_boards
+
+def timestamp_resolver(obj, info):
+    return str(datetime.datetime.now())
 
 def tasks_resolver(obj, info):
     tasks = eager_find_all_tasks()
