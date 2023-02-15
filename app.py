d44244caf5584a48eb5a3a53277e745ca2f043b3
peter@output.sk <peter@output.sk>
2023-02-15 21:09:19 +0200

    Update resolvers

--- app.py
+++ app.py
@@ -12,19 +12,25 @@ from flask import request, jsonify
 from api.queries import (
     boards_resolver,
     tasks_resolver,
+    timestamp_resolver,
 )
 from api.mutations import (
     create_task_resolver,
     move_task_resolver,
+    delete_task_resolver,
+    create_board_resolver,
 )
 
 query = ObjectType("Query")
+query.set_field("timestamp", timestamp_resolver)
 query.set_field("tasks", tasks_resolver)
 query.set_field("boards", boards_resolver)
 
 mutation = ObjectType("Mutation")
+mutation.set_field("createBoard", create_board_resolver)
 mutation.set_field("createTask", create_task_resolver)
 mutation.set_field("moveTask", move_task_resolver)
+mutation.set_field("deleteTask", delete_task_resolver)
 
 type_defs = load_schema_from_path("schema.graphql")
 schema = make_executable_schema(
